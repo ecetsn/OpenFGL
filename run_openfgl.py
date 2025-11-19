f9e3962ea4dcf8e77a7cd04a41dccb7f0ed8b22c
Ece Tosun <ece.tosun@sabanciuniv.edu>
2025-11-19 21:58:17 +0300

    Reproduce Init.

---
+++ run_openfgl.py
@@ -0,0 +1,28 @@
+import openfgl.config as config
+
+
+from openfgl.flcore.trainer import FGLTrainer
+
+args = config.args
+
+args.root = "./dataset"
+
+args.dataset = ["Cora"]
+args.simulation_mode = "subgraph_fl_louvain"
+args.num_clients = 10
+
+
+if True:
+    args.fl_algorithm = "fedavg"
+    args.model = ["gcn"]
+else:
+    args.fl_algorithm = "fedproto"
+    args.model = ["gcn", "gat", "sgc", "mlp", "graphsage"] # choose multiple gnn models for model heterogeneity setting.
+
+args.metrics = ["accuracy"]
+
+
+
+trainer = FGLTrainer(args)
+
+trainer.train()
